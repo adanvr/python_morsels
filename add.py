9294094e9e40b242d5c7d96c826f3ac3587ce279
Adan Romero <Adan.V.Romero@kp.org>
2018-10-10 09:38:16 -0700

    Updated new problems

---
+++ add.py
@@ -0,0 +1,6 @@
+def add(matrix1, matrix2):
+    """Add corresponding numbers in given 2-D matrices."""
+    return [
+        [n + m for n, m in zip(row1, row2)]
+        for row1, row2 in zip(matrix1, matrix2)
+    ]
